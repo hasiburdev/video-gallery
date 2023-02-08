a4afd31f2f689b0eb43f02a05b77bbe57ba39e7d
Hasibur Rahman <90032528+hasiburdev@users.noreply.github.com>
2023-02-08 16:41:44 +0600

    Initial commit

---
+++ src/components/videos/Videos.js
@@ -0,0 +1,16 @@
+import { useGetVideosQuery } from "../../rtk/features/api/apiSlice";
+import Video from "./Video";
+import VideoLoader from "../ui/loaders/VideoLoader";
+import Error from "../ui/Error";
+
+export default function Videos() {
+  const { data: videos, isLoading, isError } = useGetVideosQuery();
+  let content = null;
+  if (isLoading) content = <VideoLoader />;
+  if (!isLoading && isError) content = <Error message="There was an error!" />;
+  if (!isLoading && !isError && videos?.length === 0)
+    content = <Error message="No videos found!" />;
+  if (!isLoading && !isError && videos?.length > 0)
+    content = videos.map((video) => <Video key={video.id} video={video} />);
+  return <>{content}</>;
+}
---
+++ src/rtk/features/api/apiSlice.js
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:9000",
+  }),
+  endpoints: (builder) => ({
+    getVideos: builder.query({
+      query: () => "/videos",
+    }),
+    getVideo: builder.query({
+      query: (videoId) => `/videos/${videoId}`,
+    }),
+    getRelatedVideos: builder.query({
+      query: ({ id, title }) => {
+        const tags = title.split(" ");
+        const likes = tags.map((tag) => `title_like=${tag}`).join("&");
+        const queryString = `/videos?${likes}&ne=${id}&_limit=4`;
+        return queryString;
+      },
+    }),
+    addVideo: builder.mutation({
+      query: (data) => ({
+        url: "/videos",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetVideosQuery,
+  useGetVideoQuery,
+  useGetRelatedVideosQuery,
+  useAddVideoMutation,
+} = apiSlice;
